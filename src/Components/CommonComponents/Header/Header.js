4fb023b8235d30cac13b9f8dc61d35a7e5b3442a
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-03-27 11:20:11 +0330

    Add Logic For Show And Hide SearchBox

--- src/Components/CommonComponents/Header/Header.js
+++ src/Components/CommonComponents/Header/Header.js
@@ -1,19 +1,17 @@
 // !Imports
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 // !Components
-import Navbar from './Navbar/Navbar'
-import SearchModal from './SearchModal/SearchModal'
-import Basket from './Basket/Basket'
- function Header() {
+import Navbar from "./Navbar/Navbar";
+import Basket from "./Basket/Basket";
+function Header() {
   return (
     <div>
-      <Navbar/>
-      <SearchModal/>
-      <Basket/>
+      <Navbar />
+      <Basket />
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/Components/CommonComponents/Header/Navbar/Navbar.js
+++ src/Components/CommonComponents/Header/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+// !Importe
 import React from "react";
 import "./Navbar.css";
 // !icons
@@ -6,11 +7,28 @@ import { FaUserPlus } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import { FaBasketShopping } from "react-icons/fa6";
 
+// !Componnet
 function Navbar() {
-  const myRef = React.createRef();
+  // !ElementsRef
+  const mobileMenuRef = React.createRef();
+  const searchBoxRef = React.createRef();
+
+  // !Functions
   const showAndHideMobileMenu = () => {
-    let mobileMenuElement = myRef.current;
-    mobileMenuElement.classList.toggle('open')
+    let mobileMenuElement = mobileMenuRef.current;
+    mobileMenuElement.classList.toggle("open");
+  };
+  //!
+
+  const searchBoxShowHide = () => {
+    let searchBoxELement = searchBoxRef.current;
+    searchBoxELement.classList.toggle("open");
+  };
+  //!
+
+  const closeModal = () => {
+    let searchBoxELement = searchBoxRef.current;
+    searchBoxELement.classList.toggle("open");
   };
 
   return (
@@ -59,7 +77,7 @@ function Navbar() {
             </a>
           </span>
 
-          <span className="navbar__btns__search">
+          <span className="navbar__btns__search" onClick={searchBoxShowHide}>
             <IoSearch className="navbar__btns__search-icon" />
           </span>
 
@@ -68,17 +86,17 @@ function Navbar() {
           </span>
         </div>
 
-        <div className="mobile__menu" ref={myRef} >
-        <div className="navbar__logo">
-          <span className="navbar__logo-icon">
-            <FaShopify className="navbar__logo-icon-shop" />
-          </span>
-          <span className="navbar__logo-text">
-            <a href="#" className="navbar__logo-text-link">
-              Shop Website
-            </a>
-          </span>
-        </div>
+        <div className="mobile__menu" ref={mobileMenuRef}>
+          <div className="navbar__logo">
+            <span className="navbar__logo-icon">
+              <FaShopify className="navbar__logo-icon-shop" />
+            </span>
+            <span className="navbar__logo-text">
+              <a href="#" className="navbar__logo-text-link">
+                Shop Website
+              </a>
+            </span>
+          </div>
           <ul className="mobile__menu__list">
             <li className="mobile__menu__list__item">
               <a href="#" className="mobile__menu__list__item-link">
@@ -116,6 +134,34 @@ function Navbar() {
               </a>
             </li>
           </ul>
+        </div>
+
+        <div className="search-modal" ref={searchBoxRef}>
+          <span className="search-modal__close" onClick={closeModal}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </span>
+          <div className="search-modal__wrapper">
+            <span className="search-modal__title">Search Prodcuts </span>
+            <input
+              type="text"
+              className="search-modal__input"
+              placeholder="Enter Product Name"
+            />
+            <button className="search-modal-btn">Search</button>
+          </div>
         </div>
 
         <span
