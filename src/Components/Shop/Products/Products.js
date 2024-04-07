ed54a5f2302d291c8e6156b66a94c2ddddcd124d
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-04-07 11:23:20 +0330

    [Products]- Add Css Style And html Template For Products Component

---
+++ src/Components/Shop/Products/Products.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { useState } from "react";
+import "./Products.css";
+import Product from "./Product/Product";
+function Products() {
+  let [productsDatas, setproductsDatas] = useState([
+    {
+      id: 1,
+      name: "shirt",
+      price: "100",
+      rate: "4.2",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 2,
+      name: "cup",
+      price: "200",
+      rate: "2.2",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 3,
+      name: "book",
+      price: "1000",
+      rate: "1.2",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+    {
+      id: 4,
+      name: "jeweley",
+      price: "800",
+      rate: "5",
+      image: "./images/product-1.jpg",
+    },
+  ]);
+  return (
+    <div className="products">
+      <div className="container">
+        <div className="products__wrapper">
+          {productsDatas.map((product) => (
+            <Product {...product}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Products;
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -3,12 +3,14 @@ import './Shop.css'
 import Header from '../CommonComponents/Header/Header'
 import ShopLanding from './ShopLanding/ShopLanding'
 import ShopOption from './ShopOption/ShopOption'
+import Products from './Products/Products'
  function Shop() {
   return (
     <div className='shop'>
       <Header/>
       <ShopLanding/>
       <ShopOption/>
+      <Products/>
       
     </div>
   )
