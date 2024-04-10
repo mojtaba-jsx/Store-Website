ee304243658f1d03f4470189944f8476b3a7b2c3
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-04-10 12:31:48 +0330

    [ProductInfo]- Add Footer To ProductInfo page

--- src/Components/ProductInfo/ProductInfo.js
+++ src/Components/ProductInfo/ProductInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./ProductInfo.css";
 import Navbar from "../CommonComponents/Header/Navbar/Navbar";
 import ProductInfoRoute from "./ProductInfoRoute/ProductInfoRoute";
+import Footer from "../CommonComponents/Footer/Footer";
 import { FaCartArrowDown } from "react-icons/fa6";
 import { FaPencilAlt } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
@@ -26,7 +27,7 @@ function ProductInfo() {
             4.2 <FaStar className="product-info__rate-icon" />
           </span>
           <p className="product-info__description">
-          <FaPencilAlt className="product-info__description-icon" />
+            <FaPencilAlt className="product-info__description-icon" />
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
             veritatis quibusdam eaque obcaecati. Aliquam tenetur ad nihil velit
             omnis quidem.
@@ -34,13 +35,19 @@ function ProductInfo() {
           <button className="product-info__category">#Mens Clothing</button>
           <span className="product-info__stock">In Stock : 200</span>
           <div className="product-info__inputs">
-            <input type="number" value={10} className="product-info__inputs-number" />
+            <input
+              type="number"
+              value={10}
+              className="product-info__inputs-number"
+            />
             <button className="product-info__inputs-btn">
-              Add To Cart <FaCartArrowDown className="product-info__inputs-btn-icon" />
+              Add To Cart{" "}
+              <FaCartArrowDown className="product-info__inputs-btn-icon" />
             </button>
           </div>
         </div>
       </div>
+      <Footer/>
     </div>
   );
 }
