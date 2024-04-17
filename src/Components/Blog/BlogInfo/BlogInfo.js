c4794dc90bc2c11c92bdd289f6910fb3ebf02526
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-04-17 12:56:35 +0330

    [Bloginfo]- Add ShopBnefits And Footer To BlogInfo Page

--- src/Components/Blog/BlogInfo/BlogInfo.js
+++ src/Components/Blog/BlogInfo/BlogInfo.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./BlogInfo.css";
 import Navbar from "../../CommonComponents/Header/Navbar/Navbar";
 import BlogInfoLanding from "./BlogInfoLanding/BlogInfoLanding";
+import ShopBenefits from '../../Shop/ShopBenefits/ShopBenefits'
+import Footer from '../../CommonComponents/Footer/Footer'
 import { RiAdminFill } from "react-icons/ri";
 import { CiCalendarDate } from "react-icons/ci";
 import { FaTag } from "react-icons/fa6";
@@ -39,17 +41,19 @@ function BlogInfo() {
           mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis
           molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit
           libero. Pellentesque elit ullamcorper dignissim cras tincidunt.
-          Pharetra et ultrices neque ornare aenean euismod elementum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris
-          vitae ultricies leo integer malesuada nunc. In nulla posuere
-          sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices
-          mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis
-          molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit
-          libero. Pellentesque elit ullamcorper dignissim cras tincidunt.
-          Pharetra et ultrices neque ornare aenean euismod elementum.
+          Pharetra et ultrices neque ornare aenean euismod elementum. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae
+          ultricies leo integer malesuada nunc. In nulla posuere sollicitudin
+          aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus
+          imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie
+          a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero.
+          Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et
+          ultrices neque ornare aenean euismod elementum.
         </p>
       </div>
+      <ShopBenefits/>
+      <Footer/>
     </div>
   );
 }
