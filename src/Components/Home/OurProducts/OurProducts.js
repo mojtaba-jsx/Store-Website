c1cd291fbea73c2a9a91960ea075657b98c777a8
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-05-25 12:07:42 +0330

    Add Slider To our Product

--- src/Components/Home/OurProducts/OurProducts.js
+++ src/Components/Home/OurProducts/OurProducts.js
@@ -1,20 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./OurProducts.css";
 import Product from "../../Shop/Products/Product/Product";
 import { FaProductHunt } from "react-icons/fa";
-function OurProducts() {
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=4")
-      .then((res) => res.json())
-      .then((products) => {
-        setProduct(products);
-        // console.log(products);
-      });
-  });
 
-  let [product, setProduct] = useState([]);
+function OurProducts() {
+  let [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=5")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    spacing: '20',
+    slidesToShow: products.length >= 4 ? 4 : products.length, // تعداد ستون‌های اسلایدر
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024, // اندازه صفحه برای تغییر تعداد ستون‌ها
+        settings: {
+          slidesToShow: 3, // تعداد ستون‌های اسلایدر برای صفحات با اندازه 1024px
+        },
+      },
+      {
+        breakpoint: 768, // اندازه صفحه برای تغییر تعداد ستون‌ها
+        settings: {
+          slidesToShow: 2, // تعداد ستون‌های اسلایدر برای صفحات با اندازه 768px
+        },
+      },
+      {
+        breakpoint: 480, // اندازه صفحه برای تغییر تعداد ستون‌ها
+        settings: {
+          slidesToShow: 1, // تعداد ستون‌های اسلایدر برای صفحات با اندازه 480px
+        },
+      },
+    ],
+  };
 
   return (
     <div className="our-products">
@@ -22,14 +52,15 @@ function OurProducts() {
         Our Products
         <FaProductHunt />
       </h1>
-      <div className="our-products__wrapper">
-        {product.map((item) => (
-          <Product {...item} key={item.id} />
+      <Slider {...settings} className="slider">
+        {products.map((item) => (
+          <div key={item.id}>
+            <Product {...item}  className='product-slider'/>
+          </div>
         ))}
-      </div>
+      </Slider>
       <button className="our-products__btn">
         <Link className="our-products__btn-link" to={"/shop"}>
-          {" "}
           Show more
         </Link>
       </button>
